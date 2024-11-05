5d7dfa590f0341b018989e269834bf1dea72c88d
reemanq <reem.ansar@student.tdsb.on.ca>
2024-11-04 22:06:53 -0500

    Update PP1_9.py

--- PP1_9.py
+++ PP1_9.py
@@ -1,22 +1,32 @@
 
 
 def q1(): 
+  print('"Hello World"')
   #Write Assignment code here
 
 
 def q2(): 
+  d = input("Input a word: ")
+  print(d.lower())
+  print(d.upper())
   #Write Assignment code here
 
 
 def q3(): 
+  c = input("Input a word that is at least 5 letters long: ")
+  print(c[2:4])
   #Write Assignment code here
 
 
 def q4(): 
+  e = input("Input a word: ")
+  print(e.index("o"))
   #Write Assignment code here
 
 
 def q5(): 
+  g = input("Input a word: ")
+  print(len(g))
   #Write Assignment code here
 
 
@@ -24,8 +34,8 @@ def q5():
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
